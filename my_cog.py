f8cab25a2ae0af40e329e4a3fc2964ac96512765
Kouei Konoo <m23059@g.metro-cit.ac.jp>
2025-01-12 20:41:37 +0900

    add def auto_delivery

--- my_cog.py
+++ my_cog.py
@@ -1,8 +1,13 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
+import my_pd
+import my_date
 
 class MyCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.my_pandas = my_pd.MyPandas("menu_data_with_items.xlsx")
+        self.my_date = my_date.MyDate()
+        self.auto_delivery.start()  # Cogの初期化時にタスクを開始
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -21,4 +26,19 @@ class MyCog(commands.Cog):
             await message.channel.send('Hello!')
         await self.bot.process_commands(message)  # 他のコマンドが処理されるようにする
 
-    
+    @tasks.loop(seconds=1) 
+    async def auto_delivery(self):
+        self.my_date.update()  # 最新の日付と時刻を取得
+        if self.my_date.now_time == "08:00:00":  # 朝8時に通知
+            channel = self.bot.get_channel(1270216407224680458)  # 実際のチャンネルIDを指定
+            if channel:
+                menu = self.my_pandas.get_menu_for_today()
+                menu_message = "\n".join([item for item in menu if item != 'nan'])  # リストの内容を整形
+                await channel.send(f"@everyone おはようございます。本日のメニューをお知らせします。\n{menu_message}")
+
+    @auto_delivery.before_loop
+    async def before_auto_delivery(self):
+        await self.bot.wait_until_ready()  # ボットが準備完了するまで待機
+
+def setup(bot):
+    bot.add_cog(MyCog(bot))
