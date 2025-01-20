16a1267767a7d99adab7ed2c7e59953826ed1a06
Kouei Konoo <m23059@g.metro-cit.ac.jp>
2025-01-20 14:02:30 +0900

    change async def auto_delivery

--- my_cog.py
+++ my_cog.py
@@ -1,13 +1,14 @@
 from discord.ext import commands, tasks
-import my_pd
-import my_date
+from my_pd import MyPandas
+from my_date import MyDate
 
 class MyCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.my_pandas = my_pd.MyPandas("menu_data_with_items.xlsx")
-        self.my_date = my_date.MyDate()
-        self.auto_delivery.start()  # Cogの初期化時にタスクを開始
+        self.my_pandas = MyPandas("menu_data_with_items.xlsx")
+        self.my_date = MyDate()
+        self.channel_id = None  # チャンネルIDを初期化
+        self.auto_delivery_started = False
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -26,11 +27,11 @@ class MyCog(commands.Cog):
             await message.channel.send('Hello!')
         await self.bot.process_commands(message)  # 他のコマンドが処理されるようにする
 
-    @tasks.loop(seconds=1) 
+    @tasks.loop(seconds=0.1)
     async def auto_delivery(self):
         self.my_date.update()  # 最新の日付と時刻を取得
-        if self.my_date.now_time == "08:00:00":  # 朝8時に通知
-            channel = self.bot.get_channel(1270216407224680458)  # 実際のチャンネルIDを指定
+        if self.my_date.now_time == "08:00:00" and self.channel_id:
+            channel = self.bot.get_channel(self.channel_id)  # 実際のチャンネルIDを指定
             if channel:
                 menu = self.my_pandas.get_menu_for_today()
                 menu_message = "\n".join([item for item in menu if item != 'nan'])  # リストの内容を整形
@@ -39,6 +40,12 @@ class MyCog(commands.Cog):
     @auto_delivery.before_loop
     async def before_auto_delivery(self):
         await self.bot.wait_until_ready()  # ボットが準備完了するまで待機
+
+    def set_channel_id(self, channel_id):
+        self.channel_id = channel_id
+        if not self.auto_delivery_started:
+            self.auto_delivery.start()
+            self.auto_delivery_started = True
 
 def setup(bot):
     bot.add_cog(MyCog(bot))
